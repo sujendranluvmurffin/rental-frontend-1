dd0a901e9fa8dd17174aa673f5ff92cced9e2ab2
sujendranluvmurffin <sujendranluvmurffin@gmail.com>
2025-09-27 12:47:07 +0530

    Updated HostDashboard.tsx

--- src/pages/HostDashboard.tsx
+++ src/pages/HostDashboard.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from 'react';
-import { Plus, Package, DollarSign, Calendar, Eye, Edit, Trash2 } from 'lucide-react';
+import { Plus, Package, DollarSign, Calendar, Eye, CreditCard as Edit, Trash2 } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { Button } from '../components/ui/button';
 import { Badge } from '../components/ui/badge';
